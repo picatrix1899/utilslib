e47704735cd463b44bd2bbba0b9deb83459df168
fz <fz@nbwksjoh.active.local>
2016-12-12 16:40:22 +0100

    Change-Id: I3b52f78bd7ab1b6911fe088e988149f9ed3d0457

--- src/cmn/picatrix1899/utilslib/color/Color4.java
+++ src/cmn/picatrix1899/utilslib/color/Color4.java
@@ -2,7 +2,18 @@ package cmn.picatrix1899.utilslib.color;
 
 public interface Color4 extends Color3
 {
+	
+	/**
+	 * Gets the alpha component as interger component(range from 0 to 255) 
+	 * 
+	 * @return The alpha component as interger component value(range from 0 to 255)
+	 */
 	public abstract int getA();
 	
+	/**
+	 * Gets the alpha component as unity component(range from 0 to 1) 
+	 * 
+	 * @return The alpha component as unity component value(range from 0 to 1)
+	 */
 	public abstract float getUnityA();
 }
