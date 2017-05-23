49a7cfe3adc26fe3acaba9d6220e7a91f8cfa4e2
picatrix1899 <FlorianZilkenat@gmx.de>
2017-05-23 17:47:17 +0200

    .

--- utilslib/src/Main.java
+++ utilslib/src/Main.java
@@ -4,13 +4,19 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		PluginSystemExample.main();
+		float pos = 50.0f;
+		
+		float minPos = 0.0f;
+		float minVal = 0.0f;
+		float maxPos = 100.0f;
+		float maxVal = 200.0f;
+		
+		float out = 0.0f;
+		
+		out = pos * maxVal / maxPos;
+		
+		System.out.println(out);
 	}
 
-	
-	public int test()
-	{
-		return 254;
-	}
 	
 }
