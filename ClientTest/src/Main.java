5c315157ef26c3d9d053d5d05ce134750b187600
picatrix1899 <FlorianZilkenat@gmx.de>
2017-06-01 10:18:20 +0200

    .

--- ClientTest/src/Main.java
+++ ClientTest/src/Main.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
 
-import cmn.utilslib.essentials.SimpleThread;
 
 public class Main
 {
