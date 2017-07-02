8956d0e6e3ac8a8cd25865cec30ba9756d10e322
picatrix1899 <FlorianZilkenat@gmx.de>
2017-07-02 22:13:28 +0200

    .

--- utilslib/src/cmn/utilslib/math/BTree.java
+++ utilslib/src/cmn/utilslib/math/BTree.java
@@ -2,11 +2,19 @@ package cmn.utilslib.math;
 
 import java.util.Comparator;
 
+import cmn.utilslib.math.vector.Vector2f;
+
 public class BTree<V,D>
 {
 	private BTreeNode<D> main;
 	
-	private Comparator<V> c;
+	private Comparator<Vector2f> c = (a, b) ->
+	{
+		
+		
+		
+		return 0;
+	};
 	
 	public void add(float value, D data)
 	{
