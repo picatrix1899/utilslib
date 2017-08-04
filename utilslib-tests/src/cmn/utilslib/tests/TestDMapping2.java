1ac6f690a4de262ce9efc818d8635fba547db9f8
picatrix1899 <FlorianZilkenat@gmx.de>
2017-08-04 17:42:10 +0200

    .

--- utilslib-tests/src/cmn/utilslib/tests/TestDMapping2.java
+++ utilslib-tests/src/cmn/utilslib/tests/TestDMapping2.java
@@ -289,4 +289,201 @@ public class TestDMapping2 implements IUnitTest
 		
 		return true;
 	}
+	
+	
+	@IUnitTest.Test("getFirstByA(A)")
+	public boolean testGetFirstByA()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(3, 8);
+		map.add(2, 7);
+		map.add(3, 6);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(3, 2);
+		map.add(8, 1);
+		map.add(3, 0);
+		
+		IDMap2Base<Integer,Integer> r = map.getFirstByA(3);
+		
+		return r.getB() == 8;
+	}
+	
+	@IUnitTest.Test("getFirstByB(B)")
+	public boolean testGetFirstByB()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(1, 3);
+		map.add(2, 7);
+		map.add(3, 3);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(7, 3);
+		map.add(8, 1);
+		map.add(9, 3);
+		
+		IDMap2Base<Integer,Integer> r = map.getFirstByB(3);
+		
+		return r.getA() == 1;
+	}
+	
+	@IUnitTest.Test("getBByA(A)")
+	public boolean testGetBByA()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(3, 8);
+		map.add(2, 7);
+		map.add(3, 6);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(3, 2);
+		map.add(8, 1);
+		map.add(3, 0);
+		
+		List<Integer> r = map.getBByA(3);
+		
+		if(!(r.size() == 4)) return false;
+		if(!(r.get(0) == 8)) return false;
+		if(!(r.get(1) == 6)) return false;
+		if(!(r.get(2) == 2)) return false;
+		if(!(r.get(3) == 0)) return false;
+		
+		return true;
+	}
+	
+	@IUnitTest.Test("getAByB(B)")
+	public boolean testGetAByB()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(1, 3);
+		map.add(2, 7);
+		map.add(3, 3);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(7, 3);
+		map.add(8, 1);
+		map.add(9, 3);
+		
+		List<Integer> r = map.getAByB(3);
+		
+		if(!(r.size() == 5)) return false;
+		if(!(r.get(0) == 1)) return false;
+		if(!(r.get(1) == 3)) return false;
+		if(!(r.get(2) == 6)) return false;
+		if(!(r.get(3) == 7)) return false;
+		if(!(r.get(4) == 9)) return false;
+		
+		return true;
+	}
+	
+	@IUnitTest.Test("getFirstBByA(A)")
+	public boolean testGetFirstBByA()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(3, 8);
+		map.add(2, 7);
+		map.add(3, 6);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(3, 2);
+		map.add(8, 1);
+		map.add(3, 0);
+		
+		int r = map.getFirstBByA(3);
+		
+		return r == 8;
+	}
+	
+	@IUnitTest.Test("getFirstAByB(B)")
+	public boolean testGetFirstAByB()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(1, 3);
+		map.add(2, 7);
+		map.add(3, 3);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(7, 3);
+		map.add(8, 1);
+		map.add(9, 3);
+		
+		int r = map.getFirstAByB(3);
+		
+		return r == 1;
+	}
+	
+	@IUnitTest.Test("size()")
+	public boolean testSize()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(1, 3);
+		map.add(2, 7);
+		map.add(3, 3);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(7, 3);
+		map.add(8, 1);
+		map.add(9, 3);
+		
+		return map.size() == 10;
+	}
+	
+	@IUnitTest.Test("contains(A,B)")
+	public boolean testContains1()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(1, 8);
+		map.add(2, 7);
+		map.add(3, 6);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(7, 2);
+		map.add(8, 1);
+		map.add(9, 0);
+		
+		return map.contains(3,6);
+	}
+	
+	@IUnitTest.Test("contains(DMap2<A,B>)")
+	public boolean testContains2()
+	{
+		IDMapping2<Integer,Integer> map = new DMapping2<Integer,Integer>();
+		
+		map.add(0, 9);
+		map.add(1, 8);
+		map.add(2, 7);
+		map.add(3, 6);
+		map.add(4, 5);
+		map.add(5, 4);
+		map.add(6, 3);
+		map.add(7, 2);
+		map.add(8, 1);
+		map.add(9, 0);
+		
+		return map.contains(new DMap2<Integer,Integer>(2,7));
+	}
 }
