ccef570f9b6964d4bcec20f5b5aa29c6676b7bd1
picatrix1899 <FlorianZilkenat@gmx.de>
2018-09-13 20:38:01 +0200

    added a list for iteration an examination of the trees leafes to the binary tree

--- utilslib/src/cmn/utilslib/math/btree/BTreeWalker.java
+++ utilslib/src/cmn/utilslib/math/btree/BTreeWalker.java
@@ -1,15 +1,21 @@
 package cmn.utilslib.math.btree;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.Iterator;
 
 public class BTreeWalker<T,E>
 {
-	public BTree<T,E> tree;
+	private BTree<T,E> tree;
+	private ArrayList<BTreeLeaf<T,E>> leafes;
+	private ArrayList<T> values;
 	
 	public BTreeWalker(BTree<T,E> tree)
 	{
 		this.tree = tree;
+		this.leafes = new ArrayList<BTreeLeaf<T,E>>();
+		this.values = new ArrayList<T>();
 	}
 	
 	public T walk(BTreeEvaluation<E> evaluation)
@@ -55,5 +61,47 @@ public class BTreeWalker<T,E>
 		}
 		
 		return null;
+	}
+	
+	public void refreshLeafList()
+	{
+		this.leafes = new ArrayList<BTreeLeaf<T,E>>();
+		
+		if(this.tree.a == null)
+		
+		iterativeWalk(leafes, this.tree);
+	}
+	
+	public Iterator<BTreeLeaf<T,E>> leafIterator()
+	{
+		return this.leafes.iterator();
+	}
+
+	public Iterator<T> valueIterator()
+	{
+		return this.values.iterator();
+	}
+	
+	private void iterativeWalk(ArrayList<BTreeLeaf<T,E>> leafes, BTreeNode<T,E> currentNode)
+	{
+		if(currentNode.a instanceof BTreeNode)
+		{
+			iterativeWalk(leafes, (BTreeNode<T,E>)currentNode.a);
+		}
+		else
+		{
+			leafes.add((BTreeLeaf<T,E>)currentNode.a);
+			values.add(((BTreeLeaf<T,E>)currentNode.a).data);
+		}
+		
+		if(currentNode.b instanceof BTreeNode)
+		{
+			iterativeWalk(leafes, (BTreeNode<T,E>)currentNode.b);
+		}
+		else
+		{
+			leafes.add((BTreeLeaf<T,E>)currentNode.b);
+			values.add(((BTreeLeaf<T,E>)currentNode.b).data);
+		}
 	}
 }
